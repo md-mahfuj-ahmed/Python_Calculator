3e9ac8a64f2635deeca93756c9f90e5c1f0764e9
Md Mahfuj Ahmed <mahfujsizan262006@gmail.com>
2026-05-16 15:02:09 +0600

    Delete if_else_elif.py

--- if_else_elif.py
+++
@@ -1,7 +0,0 @@
-marks = int(input("give me your marks please"))
-if marks < 33 :
-    print("you are fail")
-elif marks > 80 :
-    print("congrats you got gpa 5.00 ")
-else :
-    print("congratulation, you passed")
